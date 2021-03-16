dba2e7fb320681f9bd41f1841dbcc476e700202b
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-16 11:44:05 +0100

    Añadido almacenamiento persistente guardarStorage() cargarStorage() @service

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -9,15 +9,26 @@ export class DeseosService {
   listas :Lista[]=[];
 
   constructor() {
-    const lista1=new Lista("Recolectar piedras");
-    const lista2=new Lista("Recolectar agua");
+    // const lista1=new Lista("Recolectar piedras");
+    // const lista2=new Lista("Recolectar agua");
 
-    this.listas.push(lista1, lista2);
+    // this.listas.push(lista1, lista2);
+
+    this.cargarStorage();
 
 
   }
   crearLista(titulo:string){
     const nuevaLista= new Lista(titulo);
     this.listas.push(nuevaLista);
+    this.guardarStorage();
   }
+
+  guardarStorage(){
+    localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+  cargarStorage(){
+    if ( localStorage.getItem('data')) this.listas=JSON.parse(localStorage.getItem('data'));
+  }
+
 }
