1147133885b027d6ec2c4fcfb357ce40d37d69cd
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-16 12:28:58 +0100

    cambioCheck() @agregar

--- src/app/pages/agregar/agregar.page.ts
+++ src/app/pages/agregar/agregar.page.ts
@@ -29,4 +29,17 @@ export class AgregarPage implements OnInit {
     this.deseosService.guardarStorage();
   }
 
+  cambioCheck(item:ListaItem){
+    const pendientes=this.lista.items.filter(itemData=> !itemData.completado).length;
+    if (pendientes===0){
+      this.lista.terminadaEn= new Date();
+      this.lista.terminada=true;
+    }else{
+      this.lista.terminadaEn= null;
+      this.lista.terminada=false;
+    }
+    this.deseosService.guardarStorage();
+    console.log(this.deseosService.listas);
+  }
+
 }
