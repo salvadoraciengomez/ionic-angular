e7a2f929e970dd3d701230027d2fcbb626f37b08
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-16 10:17:12 +0100

    Añadida page Agregar y rutas hijas

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { DeseosService } from 'src/app/services/deseos.service';
 
 @Component({
@@ -8,8 +9,13 @@ import { DeseosService } from 'src/app/services/deseos.service';
 })
 export class Tab1Page {
 
-  constructor(public deseosService:DeseosService) {
+  constructor(public deseosService:DeseosService, private router:Router) {
 
+  }
+
+  agregarLista(){
+    // this.router.navigateByUrl('/agregar');
+    this.router.navigate(['tabs/tab1/agregar']);
   }
 
 }
