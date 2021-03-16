e271a8c4e2482f0c17efcbbac094c0f7340f9513
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-16 14:26:03 +0100

    Added Components Module for shared components.Vid132>

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListasComponent } from './listas/listas.component';
+
+
+
+@NgModule({
+  declarations: [ListasComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [ListasComponent]
+})
+export class ComponentsModule { }
