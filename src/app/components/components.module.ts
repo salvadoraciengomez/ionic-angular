afd6976f4d10175a7f01ed2a0a5c9afdf655ab8a
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-17 09:58:26 +0100

    added pipes.module & pipes/filtroCompl

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListasComponent } from './listas/listas.component';
 import { IonicModule } from '@ionic/angular';
+import { PipesModule } from '../pipes/pipes.module';
 
 
 
 @NgModule({
   declarations: [ListasComponent],
   imports: [
+    PipesModule,
     CommonModule,
     IonicModule
   ],
